b2f49371e7ef3c44282dd224282ccfd1a67345ee
dohunkwon <ehgns24@naver.com>
2022-06-20 14:25:45 +0900

    머지2

--- src/main/java/com/sparta/airbnb_clone/AirBnBCloneApplication.java
+++ src/main/java/com/sparta/airbnb_clone/AirBnBCloneApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing//타임스탬프
+@EnableJpaAuditing
 @SpringBootApplication
 public class AirBnBCloneApplication {
 
